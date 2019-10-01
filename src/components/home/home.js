9885cace9a849322215e8345952eb0c5fa35bc8d
Ram <ramkumar.gupta@quantiphi.com>
2019-10-01 13:25:01 +0530

    Added home page

---
+++ src/components/home/home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div>
+        <p>Home Page</p>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
