5ea2e00477534b23656d7d0dbecaa147ea7ef729
Ram <ramkumar.gupta@quantiphi.com>
2019-10-01 13:37:16 +0530

    Added header and footer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import HomeComponent from "./components/home/home";
+import HomeComponent from "./components/home/Home";
+import Header from "./components/partials/Header";
+import Footer from "./components/partials/Footer";
 
 function App() {
   return (
     <Router>
       <div>
+        <Header />
         <div className="dsp-flex">
           <Switch>
             <Route exact path="/" component={HomeComponent} />
           </Switch>
         </div>
+        <Footer />
       </div>
     </Router>
   );
